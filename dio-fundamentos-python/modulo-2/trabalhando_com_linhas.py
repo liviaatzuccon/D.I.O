70a59a69a3376ac2854133b98eedb9997085a26d
liviaatzuccon <livia.afonsotz@hotmail.com>
2024-08-08 13:37:12 -0300

    atualizacao

---
+++ dio-fundamentos-python/modulo-2/trabalhando_com_linhas.py
@@ -0,0 +1,14 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("Python")
+print(letras)
+
+numero = list(range(10))
+print(numero)
+
+carro = ["Ferrari", "F8", 420000, 2020, 2900, "São Paulo", True]
+print(carro)
